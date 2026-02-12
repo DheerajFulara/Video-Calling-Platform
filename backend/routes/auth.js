cfa8a8fdebdd7cc627800f010aca22ea444bbb80
DheerajFulara <dheerajfulara0207@gmail.com>
2026-02-12 11:55:54 +0530

    fix: correct middleware folder path

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -24,7 +24,7 @@
 
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
-const { authenticateToken } = require("../middleware/auth.middleware");
+const { authenticateToken } = require("../middlewares/auth.middleware");
 
 /* -------------------- VERIFY TOKEN -------------------- */
 router.get("/", authenticateToken, authController.verifyToken);
